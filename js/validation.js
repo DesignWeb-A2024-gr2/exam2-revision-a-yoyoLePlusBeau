bd57cb23b4bb9537c526dff4b52534b197db94b1
Yoan Bourassa <2442228@carrefour.cegepvicto.ca>
2024-11-20 14:35:00 -0500

    VersionFinale

--- js/validation.js
+++ js/validation.js
@@ -8,6 +8,11 @@ const sliderNote = document.getElementById('note_estime');          // Le slider
 const titreNote = document.getElementById('titre_note_estime');     // Le titre de la note estimé
 const daIconeErreur = document.getElementById('da_icone_erreur');   // L'icone d'erreur associée au input du numéro de da
 const daIconeSucces = document.getElementById('da_icone_succes');   // L'icone de succès associée au input du numéro de da
+const zoneMessage = document.getElementById("message_numero_da");
+const autre = document.getElementById("message_declaration");
+
+inputNoDA.addEventListener("input", Validation);
+sliderNote.addEventListener("input", ModifierIconeNote);
 
 // Initialisation de l'affichage de la bonne icone associé au numéro de da
 daIconeErreur.classList.remove('hidden');
@@ -21,7 +26,23 @@ function ModifierIconeNote(note) {
     // l'élément icone qui sera modifié
     const iconeNote = document.getElementById('icone_note');
     // On initialise les classes de l'élément à "vide"
-    iconeNote.setAttribute("class", "");
+    if (sliderNote.value < 20) {
+        iconeNote.setAttribute("class", "far fa-sad-cry");
+    }
+    else if (sliderNote.value < 40) {
+        iconeNote.setAttribute("class", "far fa-sad-tear");
+    }
+    else if (sliderNote.value < 60) {
+        iconeNote.setAttribute("class", "far fa-frown");
+    }
+    else if (sliderNote.value < 80) {
+        iconeNote.setAttribute("class", "far fa-smile");
+    }
+    else {
+        iconeNote.setAttribute("class", "far fa-grin-squint-tears");
+    }
+
+    titreNote.innerText = "Ma note estimée = " + sliderNote.value + " %"
 
     // Ajout des bonnes classes selon la valeur de la note
     // À COMPLÉTER
@@ -32,9 +53,14 @@ function ModifierIconeNote(note) {
  * @param {HTMLElement} element L'élément html de départ
  * @param {string} message Le message à afficher
  */
-function AfficherMessage(element, message = '') {
-    const zoneMessage = element.parentElement.querySelector('small');
-    zoneMessage.innerHTML = message;
+function AfficherMessage() {
+    if (!Validation()) {
+        
+    }
+    
+    if (declaration.checked == false) {
+        
+    }
 }
 
 /**
@@ -47,4 +73,75 @@ function ObtenirNombreAleatoire(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function Validation() {
+    zoneMessage.innerHTML = "";
+    let a = ContientChiffre();
+    let b = NbCaracteres();
+    let c = PremierCaractere();
+
+    if (a && b && c) {
+        daIconeErreur.style.display = "none"
+        daIconeSucces.style.display = "block"
+        return true;
+    }
+    else {
+        daIconeErreur.style.display = "block"
+        daIconeSucces.style.display = "none"
+        return false;
+    }
+}
+
+function ContientChiffre() {
+    if (REGEX_SEULEMENT_CHIFFRE.test(inputNoDA.value)) {
+        return true;
+    }
+    else {
+        
+        zoneMessage.innerHTML += "Le numéro de DA n'est pas uniquement composé de chiffre <br>";
+        return false;
+    }
+}
+
+function NbCaracteres() {
+    if (inputNoDA.value.length == 7) {
+        return true;
+    }
+    else {
+        zoneMessage.innerHTML += "Le numéro de DA n'a pas une longeur de 7 chiffres <br>";
+        return false;
+    }
+}
+
+function PremierCaractere() {
+    if (inputNoDA.value[0] == 1 || inputNoDA.value[0] == 2) {
+        return true;
+    }
+    else {
+        
+        zoneMessage.innerHTML += "Le numéro de DA doit commencer par un 1 ou un 2 <br>";
+        return false;
+    }
+}
+
+function Soumettre() {
+    zoneMessage.innerHTML = "";
+    if (Validation() && declaration.checked == true) {
+        return true;
+    }
+
+    if (declaration.checked == true) {
+        autre.innerHTML = "";
+    }
+
+    if (declaration.checked == false) {
+        autre.innerHTML = "La case n'est pas coché <br>";
+        return false;
+    }
+    
+    else {
+        return false;
+        
+    } 
 }
